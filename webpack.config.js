9aa6c8a7103dc4d6171cdaa3888a08ad90c538ea
smemainLi <smemainLi@gmail.com>
2018-09-20 10:34:36 +0800

    ignore file & webpack.config.js & app.vue & entry file index.js & assets resource

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const {
+    VueLoaderPlugin
+} = require('vue-loader')
+
+module.exports = {
+    /* mode: 'production', */
+    mode: 'development',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name]-aaa.[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
